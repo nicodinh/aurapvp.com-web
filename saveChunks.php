ecf97c37c8f7fa844c974540c5ebaf927b6f9620
AZARI Nicolas <nicodinh@azari.fr>
2015-01-21 13:14:55 +0100

    execute command with rcon

---
+++ saveChunks.php
@@ -0,0 +1,28 @@
+<?php
+	// Rcon
+	define( 'MQ_SERVER_ADDR', 'localhost' );
+	// rcon port
+	define( 'MQ_SERVER_PORT', 25575 );
+	// rcon password
+	define( 'MQ_SERVER_PASS', '' );
+	define( 'MQ_TIMEOUT', 2 );
+	require '/path/to/lib/MinecraftRcon.class.php';
+	try     
+	{                                                
+		$Rcon = new MinecraftRcon;                                                
+		$Rcon->Connect( MQ_SERVER_ADDR, MQ_SERVER_PORT, MQ_SERVER_PASS, MQ_TIMEOUT );                                                
+		$Data = $Rcon->Command("save-all");
+		if( $Data === false )
+		{
+			throw new MinecraftRconException( "Failed to get command result." );
+		}
+		else if( StrLen($Data) == 0 )
+		{
+			throw new MinecraftRconException( "Got command result, but it's empty." );
+		}
+	}
+	catch(MinecraftRconException $e)
+	{
+	}
+	$Rcon->Disconnect();
+?>
